3cbba77b0989dbdd3bcf91880582f992307be769
sergey <sergey.exu@gmail.com>
2015-05-27 13:27:28 +0000

    added urlManager

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -28,6 +28,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
+            ],
+        ],
     ],
     'params' => $params,
 ];
