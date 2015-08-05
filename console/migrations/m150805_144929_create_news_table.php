7d20a87065c1bae53722758b1c131de6fc63d0fc
sergey <sergey.exu@gmail.com>
2015-08-05 14:58:43 +0000

    add migration news

---
+++ console/migrations/m150805_144929_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150805_144929_create_news_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+ 
+        $this->createTable('{{%news}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . '(256) NOT NULL',
+            'summary' => Schema::TYPE_TEXT,
+            'text' => Schema::TYPE_TEXT,
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'publish_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'meta_title' => Schema::TYPE_STRING . '(128) NOT NULL',
+            'meta_description' => Schema::TYPE_STRING . '(256) NOT NULL',
+            'alias' => Schema::TYPE_STRING . '(128) NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%news}}');
+    }
+    
+}
