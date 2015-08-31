e2291b41dc296b7ff322305a9f84b38b1658dbff
sergey <sergey.exu@gmail.com>
2015-08-31 07:14:08 +0000

    added url sufix

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -34,6 +34,7 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'suffix' => '/',
             'rules' => [
                 
                 '' => 'page/index',
