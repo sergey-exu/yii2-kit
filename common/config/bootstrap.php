d1ca13ce2187653a056ed7ce12d5b7ca9b9c78f3
sergey <sergey.exu@gmail.com>
2016-03-14 07:58:05 +0000

    added storage

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,7 +3,9 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
-/*
+Yii::setAlias('@storage', dirname(dirname(__DIR__)) . '/storage');
+
+// Url Aliases
 Yii::setAlias('@frontendUrl', $_SERVER['HTTP_HOST']);
 Yii::setAlias('@backendUrl', $_SERVER['HTTP_HOST'].'/backend');
-*/
+Yii::setAlias('@storageUrl', $_SERVER['HTTP_HOST'].'/storage');
